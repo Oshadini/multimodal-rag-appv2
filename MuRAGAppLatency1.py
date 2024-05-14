7c76e2056de52a1036d2544b2653924d3abb4d30
Oshadini Kariyawasam <43576871+Oshadini@users.noreply.github.com>
2024-05-15 03:11:50 +0530

    Update MuRAGAppLatency1.py

--- MuRAGAppLatency1.py
+++ MuRAGAppLatency1.py
@@ -125,14 +125,14 @@ if uploaded_file is not None:
 
     # Categorize elements by type
     @st.cache_data(show_spinner=False)
-    def categorize_elements(raw_pdf_elements):
+    def categorize_elements(_raw_pdf_elements):
       """
       Categorize extracted elements from a PDF into tables and texts.
       raw_pdf_elements: List of unstructured.documents.elements
       """
       tables = []
       texts = []
-      for element in raw_pdf_elements:
+      for element in _raw_pdf_elements:
           if "unstructured.documents.elements.Table" in str(type(element)):
               tables.append(str(element))
           elif "unstructured.documents.elements.CompositeElement" in str(type(element)):
