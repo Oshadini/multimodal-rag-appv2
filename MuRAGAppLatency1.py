0f9091a4a345172eb0722d7faca046235fa624e7
Oshadini Kariyawasam <43576871+Oshadini@users.noreply.github.com>
2024-05-15 03:50:36 +0530

    Update MuRAGAppLatency1.py

--- MuRAGAppLatency1.py
+++ MuRAGAppLatency1.py
@@ -101,7 +101,7 @@ if uploaded_file is not None:
         file.write(uploaded_file.getvalue())
    
     image_path = "./"
-    
+    image_path2= "./figures"
     #shutil.rmtree(image_path2)
     #st.success(f"Folder '{image_path2}' and its contents have been successfully removed.")
     def list_files_in_folder(folder_path):
