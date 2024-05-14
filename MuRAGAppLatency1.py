5fccdf796e58929475958efde3117fc655ec806c
Oshadini Kariyawasam <43576871+Oshadini@users.noreply.github.com>
2024-05-15 04:19:25 +0530

    Update MuRAGAppLatency1.py

--- MuRAGAppLatency1.py
+++ MuRAGAppLatency1.py
@@ -474,6 +474,7 @@ if uploaded_file is not None:
         vectorstore = Chroma(collection_name="mm_rag_mistral04",embedding_function=OpenAIEmbeddings(openai_api_key = openai.api_key))
         retriever_multi_vector_img=create_multi_vector_retriever(vectorstore,text_summaries,texts,table_summaries,tables,image_summaries,img_base64_list)
         chain_multimodal_rag = multi_modal_rag_chain(retriever_multi_vector_img)
+        st.write(chain_multimodal_rag)
         docs = retriever_multi_vector_img.get_relevant_documents(question, limit=1)
         st.write(docs)
     
